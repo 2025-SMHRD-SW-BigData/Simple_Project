97de7817b6e963bad6c9c36509fc1c79de6f9018
br <baklook327@gmail.com>
2025-06-26 14:22:18 +0900

    로그인/회원가입

--- SEESEA/src/Server/JoinServer.js
+++ SEESEA/src/Server/JoinServer.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2')
 //위치주의
 const cors = require('cors')
 app.use(cors())
-
+app.use(express.json())
 
 let conn = mysql.createConnection({
     host : 'project-db-campus.smhrd.com', //='0.0.0.0'
@@ -20,29 +20,42 @@ let conn = mysql.createConnection({
    conn.connect();
 
 //http://localhost:3001/join
-app.get('/join', (req,res)=>{
+app.post('/join', (req,res)=>{
     console.log('접근 확인')
 
-    const JoinId = req.query.USER_ID
-    const JoinPw = req.query.PW
-    const JoinName = req.query.NAME
-    const JoinAge = req.query.AGE
+    const JoinId = req.body.USER_ID
+    const JoinPw = req.body.PW
+    const JoinName = req.body.NAME
+    const JoinAge = req.body.AGE
 
- 
+console.log(req.body)
+
+let checkID = 'SELECT * FROM Member WHERE USER_ID = ?'
+conn.query(checkID, [JoinId], (err, results) => {
+    if(err){
+        console.error(err);
+        return res.status(500).send({ success: false, message: '서버 오류' });
+    }
+
+    if(results.length > 0){
+        // 이미 존재하는 아이디
+     return res.status(400).send({ success: false, message: '이미 존재하는 아이디입니다.' });
+    }
+
 
     let sql = 'insert into Member(USER_ID, PW, NAME, AGE) values (?, ?, ?, ?)';
     conn.query(sql,[JoinId,JoinPw,JoinName,JoinAge],(err,rows)=>{
          if(!err){
-            console.log('입력성공')
-            
+            console.log('입력성공')  
+            res.send({ success: true, message: '회원가입 성공! 로그인 해주세요.' });          
         } else{
-            console.log('입력실패') 
-                       
+            console.log(err)
+            console.log('입력실패')             
         }      
     })
 
 })
-
+})
 app.listen(3001, () => {
     console.log('서버 실행 중')
-}) 
+})
--- SEESEA/src/Server/LoginServer.js
+++ SEESEA/src/Server/LoginServer.js
@@ -1,14 +1,53 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const mysql = require('mysql2')
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+let conn = mysql.createConnection({
+    host : 'project-db-campus.smhrd.com', //='0.0.0.0'
+    port : 3307,
+    user : 'campus_25SW_BigData_p2_4',
+    password : 'smhrd4',
+    database : 'campus_25SW_BigData_p2_4'
+})
+
+   conn.connect();
+
+
 
 //http://localhost:3001/login
 app.post('/login', (req,res)=>{
     console.log('접근 확인')
-    res.send('Test')
-})
+    console.log('받은 데이터:', req.body)
 
-app.listen(3001)
+    const JoinId = req.body.USER_ID
+    const JoinPw = req.body.PW
+
+
+let checkID = 'SELECT * FROM Member WHERE USER_ID = ? and PW = ?'
+conn.query(checkID, [JoinId, JoinPw], (err, results) => {
+if(err){
+    console.error(err);
+    return res.status(500).send({ success: false, message: '서버 오류' });
+    }
+
+if (results.length > 0) {
+    console.log('로그인 성공');
+    return res.send({ success: true, message: '로그인 성공' })
+}else{
+    console.log('로그인 실패')
+    return res.status(401).send({ success: false, message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
+    }
+
+console.log(req)
+
+
+})
+})
+app.listen(3001, () => {
+    console.log('서버 실행 중')
+})
